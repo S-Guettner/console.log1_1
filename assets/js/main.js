4713d4951216358265cc6460c3cbf43cc33c6aa1
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-01-03 12:03:59 +0100

    finish

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+let a = 12;
+let b = a * 3;
+document.write(b);
+
+
+console.log(document.URL); // gibt URL der aktuellen Seite in Konsole aus 
+
+
+
+console.log("Hello World!"); 
+
+console.log("Anton");
+
+let firstName = "Anton";
+
+console.log(firstName);
+
+let age = 28;
+
+console.log(age);
+
+let job = "trainer";
+
+console.log(job);
+
+let married = true;
+console.log(married);
+
+
+console.log(`${firstName} ist ${age} Jahre alt, von Beruf ${job} und ist verheiratet: ${married}   `);
